9ceb3bf8183fbbac87bbdc9759d8c6a21f2ca10d
Sam Natale <samnatale3@gmail.com>
2021-12-30 22:49:11 +0000

    Implemented a button to sign-in anonymously and a LoginButton class to define future buttons

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutterquiz/services/auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -7,7 +9,66 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("LoginScreen"),
+        title: const Text("Login"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          // Place buttons in middle of the screen
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+          children: [
+            // Logo for top of the buttons
+            const FlutterLogo(
+              size: 150,
+            ),
+            Flexible(
+              child: LoginButton(
+                icon: FontAwesomeIcons.userNinja,
+                text: 'Continue as Guest',
+                loginMethod: AuthService().anonLogin,
+                color: Colors.deepPurple,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String text;
+  final Function loginMethod;
+
+  const LoginButton(
+      {Key? key,
+      required this.color,
+      required this.icon,
+      required this.text,
+      required this.loginMethod})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ElevatedButton.icon(
+        icon: Icon(
+          icon,
+          color: Colors.white,
+          size: 20,
+        ),
+        // Different background based on the input provided to the widget
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(24),
+          backgroundColor: color,
+        ),
+        onPressed: () => loginMethod(),
+        label: Text(text),
       ),
     );
   }
