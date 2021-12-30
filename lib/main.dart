34548c6417086d1defaf926eb63e52fe6a08934f
Sam Natale <samnatale3@gmail.com>
2021-12-30 20:35:01 +0000

    Initialised Firebase with FutureBuilder used to change what is shown to the user depending on the state of Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+// Import firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const App());
 }
 
@@ -12,8 +16,33 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  // Not called within the build method as we only want to initialize once
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // FutureBuilder used to show different states based on whether or not
+    // Firebase is available.
+    return FutureBuilder(
+      // Initialize FlutterFire (Flutter Firebase)
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          // TODO: Add Error Screen
+          return MaterialApp(home: Container(child: Text('Error')));
+        }
+
+        // Once connected show the application
+        if (snapshot.connectionState == ConnectionState.done) {
+          // TODO: Add MaterialApp and AppRoutes
+          return MaterialApp(home: Container(child: Text('Connected')));
+        }
+
+        // If not connected and no errors then show loading
+        // TODO: Add LoadingScreen
+        return MaterialApp(home: Container(child: Text('Loading')));
+      },
+    );
   }
 }
