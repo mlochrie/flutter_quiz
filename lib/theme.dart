c9150ab6494e78d27e564f913ed1e5a5ecd1d16c
Sam Natale <samnatale3@gmail.com>
2021-12-30 22:21:38 +0000

    Added custom font to theme, globally used by app

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 var appTheme = ThemeData(
-    brightness: Brightness.dark,
-    textTheme: const TextTheme(
-        bodyText1: TextStyle(fontSize: 18),
-        bodyText2: TextStyle(fontSize: 16),
-        button: TextStyle(
-          letterSpacing: 1.5,
-          fontWeight: FontWeight.bold,
-        )));
+  fontFamily: GoogleFonts.nunito().fontFamily, // Global font
+
+  brightness: Brightness.dark,
+
+  textTheme: const TextTheme(
+    bodyText1: TextStyle(fontSize: 18),
+    bodyText2: TextStyle(fontSize: 16),
+    button: TextStyle(
+      letterSpacing: 1.5,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+);
