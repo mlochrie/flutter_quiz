42abc070b44b65563b4ccb81495ac7fc0625d23a
Sam Natale <samnatale3@gmail.com>
2021-12-30 22:33:06 +0000

    Implemented SteamBuilder to monitor if user is logged in

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutterquiz/screens/login/login.dart';
+import 'package:flutterquiz/screens/topics/topics.dart';
+import 'package:flutterquiz/services/auth.dart';
+
+// Check users authentication state and if they are logged in show the topics
+// screen, otherwise show login screen
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("HomeScreen"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text(
-            "Topics",
-            style: Theme.of(context).textTheme.button,
-          ),
-          onPressed: () => Navigator.pushNamed(context, '/topics'),
-        ),
-      ),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // If waiting, still loading data
+          return const Text('Loading');
+        } else if (snapshot.hasError) {
+          // If error, show error screen
+          return const Text('Error');
+        } else if (snapshot.hasData) {
+          // The user must be logged in otherwise snapshot would be null
+          return const TopicsScreen();
+        } else {
+          // The user musn't be logged in as no data
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
