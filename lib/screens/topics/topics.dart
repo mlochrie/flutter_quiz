4d97cfdffa243a37c2ef572e7922f02b4b0f3f4c
Sam Natale <samnatale3@gmail.com>
2022-01-02 21:39:24 +0000

    Implemented loading data from Firestore and displaying it as text in the form of a GridView with 2 columns

--- lib/screens/topics/topics.dart
+++ lib/screens/topics/topics.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterquiz/services/services.dart';
 import 'package:flutterquiz/shared/bottom_nav.dart';
 
 class TopicsScreen extends StatelessWidget {
@@ -6,11 +7,35 @@ class TopicsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("TopicsScreen"),
-      ),
-      bottomNavigationBar: const BottomNavBar(),
+    return FutureBuilder<List<Topic>>(
+      future: FirestoreService().getTopics(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // TODO: Display loading screen
+          return CircularProgressIndicator.adaptive();
+        } else if (snapshot.hasError) {
+          // TODO: Display error screen
+          return Text(snapshot.error.toString());
+        } else if (snapshot.hasData) {
+          var topics = snapshot.data!;
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text("TopicsScreen"),
+            ),
+            bottomNavigationBar: const BottomNavBar(),
+            body: GridView.count(
+              primary: false,
+              crossAxisCount: 2,
+              crossAxisSpacing: 10.0,
+              padding: const EdgeInsets.all(20.0),
+              children: topics.map((topic) => Text(topic.title)).toList(),
+            ),
+          );
+        } else {
+          // No topics have been found
+          return const Text('No topics have been found');
+        }
+      },
     );
   }
 }
