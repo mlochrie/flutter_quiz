a22e06eb8385aeb30ac70fdd70a59547cf7c9835
Sam Natale <samnatale3@gmail.com>
2022-01-03 14:27:12 +0000

    Implemented StreamProvider to listen to the report stream

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutterquiz/routes.dart';
+import 'package:flutterquiz/services/services.dart';
 import 'package:flutterquiz/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,12 +38,16 @@ class _AppState extends State<App> {
           }
 
           // Once connected show the application
-
           if (snapshot.connectionState == ConnectionState.done) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              routes: appRoutes,
-              theme: appTheme,
+            return StreamProvider(
+              // Function to return stream we want to use
+              create: (_) => FirestoreService().streamReport(),
+              initialData: Report(),
+              child: MaterialApp(
+                debugShowCheckedModeBanner: false,
+                routes: appRoutes,
+                theme: appTheme,
+              ),
             );
           }
 
